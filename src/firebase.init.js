00d109b3a47f305fd77da880b14352d7590925f6
Rahatul <rahatulislam9090@gmail.com>
2022-05-06 00:27:39 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpGPMyf61CcmHwSbPFnmAKZqFQo84Z_gQ",
+  authDomain: "warehouse-inventory-mana-95a43.firebaseapp.com",
+  projectId: "warehouse-inventory-mana-95a43",
+  storageBucket: "warehouse-inventory-mana-95a43.appspot.com",
+  messagingSenderId: "606875399810",
+  appId: "1:606875399810:web:3f010bf60f2f1a42ea669b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
